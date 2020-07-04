16abeeb19d4ac89506f5f21b10323dbd2ed54af9
Mateo Gonzalez <magonzalezoc@gmail.com>
2020-07-04 13:51:28 -0500

    Removed symbolic links and improved create links script

--- create_links.sh
+++ create_links.sh
@@ -2,10 +2,11 @@
 
 for env in $(ls -I create_links.sh)
 do
-	ln -s "$PWD/$env" "$HOME/.config/$env" 2>/dev/null
-	if [ $? -ne 0 ]; then
-		echo "A symlink already exists for $env"
+	target="$HOME/.config/$env"
+	if [ -f ${target} ] || [ -L ${target} ]; then
+		echo "file $env already exists on target dir. Ignoring."
 	else
+		ln -s "$PWD/$env" "$HOME/.config/$env" 2>/dev/null
 		echo "Created link for $env"
 	fi
 done
