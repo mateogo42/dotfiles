635f4f8652739a8ef3450e70a0f6d1279f8ca885
Mateo <magonzalezoc@gmail.com>
2020-07-07 08:56:20 -0500

    Spotify bar uncommented and changed i3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar top -c $HOME/.config/polybar/config.ini & 
 polybar bottom -c $HOME/.config/polybar/config.ini &  
 
 echo "Bars launched..."
